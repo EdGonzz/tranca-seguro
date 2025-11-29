02722922b9c3e68c14b31f4c7ad9a6131f5a8e3d
Edwin Contreras <ed25gonz@gmail.com>
2025-11-28 21:08:53 -0400

    feat: update password strength calculation to manipulate UI elements and lower the minimum length requirement for scoring.

--- src/tools/getPasswordStrength.ts
+++ src/tools/getPasswordStrength.ts
@@ -1,3 +1,10 @@
+const strengthSpan = document.getElementById(
+  "strength-span"
+) as HTMLSpanElement;
+const passwordStrength = document.getElementById(
+  "password-strength"
+) as HTMLDivElement;
+
 export const getPasswordStrength = (
   length: number,
   useNumbers: boolean,
@@ -5,12 +12,27 @@ export const getPasswordStrength = (
 ) => {
   let score = 0;
 
-  if (length >= 12) score++;
+  if (length >= 8) score++;
   if (useNumbers) score++;
   if (useSymbols) score++;
 
-  if (score === 0) return "Insegura";
-  if (score === 1) return "Débil";
-  if (score === 2) return "Medio";
-  if (score === 3) return "Fuerte";
+  if (score === 1) {
+    passwordStrength.classList.remove("password-strength-medium");
+    passwordStrength.classList.remove("password-strength-strong");
+    passwordStrength.classList.add("password-strength-weak");
+    strengthSpan.style.color = "red";
+    return "Débil";
+  } else if (score === 2) {
+    passwordStrength.classList.remove("password-strength-weak");
+    passwordStrength.classList.remove("password-strength-strong");
+    passwordStrength.classList.add("password-strength-medium");
+    strengthSpan.style.color = "orange";
+    return "Medio";
+  } else if (score === 3) {
+    passwordStrength.classList.remove("password-strength-weak");
+    passwordStrength.classList.remove("password-strength-medium");
+    passwordStrength.classList.add("password-strength-strong");
+    strengthSpan.style.color = "#28a745";
+    return "Fuerte";
+  }
 };
